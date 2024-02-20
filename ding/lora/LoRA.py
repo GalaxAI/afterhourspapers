bcf67ddbf2311b54af9904b78eff2d19fac74571
GalaxUniv <afterhoursbilly@gmail.com>
2024-02-20 04:54:29 +0100

    feat: rewritten LoRA

--- ding/lora/LoRA.py
+++ ding/lora/LoRA.py
@@ -1,41 +1,49 @@
 import torch
 from torch import nn
 
-class Lora(nn.Module):
-    def __init__(self, r, alpha):
+class Lora():
+    def __init__(self, lora_dropout:float,merge_weights:bool = False
+                 ,r:int = 1, alpha:int = 1,):
         super(Lora, self).__init__()
         self.r = r
         self.aplha = alpha
-        # you can add more things to lora such as dropout, merge weights.
+        if lora_dropout > 0.0:
+            self.lora_dropout = nn.Dropout(lora_dropout)
+        else: self.lora_dropout = lambda x: x # returns x
+        
+        self.merged = False
+        self.merge_weights = merge_weights
+    
 
-
-class LoraGPT2(Lora, nn.Module):
+class LoraLinear(nn.Linear,Lora):
     """
     GPT2 is using Conv1D layer for attenction weights,
     but the calculations are the same as for linear
     """
-    def __init__(self, model,r,alpha,**kwargs):
-        nn.Module.__init__(self, **kwargs)
-        Lora.__init__(self, r, alpha)
-        self.model = model
-        k,d = model.weight.shape
-        device = model.weight.device
-        self.A = nn.Parameter(self.model.weight.new_zeros((r,k)).transpose(0,1))
-        self.B = nn.Parameter(self.model.weight.new_zeros((d,r)).transpose(0,1))
-        self.scale = r/alpha
-        self.model.weight.requires_grad = False
+    def __init__(self,
+                 in_f:int,
+                 out_f:int,
+                 r:int = 1,
+                 lora_alpha:int = 1,
+                 lora_dropout:float = 0.0,
+                 merge_weights:bool = True,
+                 **kwargs):
+        
+        nn.Linear.__init__(self, in_f, out_f,**kwargs)
+        Lora.__init__(self, lora_dropout,merge_weights,r,lora_alpha)
 
-        ### Reseting the parameters
-        nn.Linear.reset_parameters(self.model)
-        # this is different than what is described in the paper but should not affect performance
+        self.A = nn.Parameter(self.weight.new_zeros((r,in_f)))
+        self.B = nn.Parameter(self.weight.new_zeros((out_f,r)))
+        self.scale = r/lora_alpha
+
+        # We are reseting the parameters for linear since we load in the weights later
+        nn.Linear.reset_parameters(self)
         nn.init.kaiming_uniform_(self.A)
         nn.init.zeros_(self.B)
     
     def forward(self, x):
-        x = self.model(x)
-        x_2 = x @ self.A @ self.B * self.scale # ABx
-        x += x_2 
-        return x
+        x = self.weight + self.B @ self.A
+        return nn.functional.linear(x, self.weight, self.bias)   
 
 class LoraEmbedding(Lora, nn.Module):
     pass
